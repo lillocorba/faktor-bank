b49daec96f34f1fd54b6d9ee852aa36031bad3cb
Agustin Corbalan <agustin.corbalan1723@gmail.com>
2020-09-17 18:37:53 -0300

    Modificaciones en JS

--- js/simulador.js
+++ js/simulador.js
@@ -1,9 +1,29 @@
-var monto = 10000 // Acá iría el valor que elige el usuario en el simulador de la web (Falta la clase de DOM)
+var monto = 10000 // Acá iría el valor que elige el usuario en el simulador de la web (Manipular HTML)
 var interes = 12 // Porcentaje de interés anual fijado previamente
-var anos = 5 // Acá iría la cantidad de años en el cual devolvería el préstamo el usuario (Falta la clase de DOM)
-var cuotaMensual // Valor de cuota mensual. Se modifica en tiempo real de acuerdo a los parámetros que elija el usuario. (Falta la clase de DOM)
-var comision // Porcentaje de comisión de acuerdo al monto del préstamo. (Falta la clase de DOM)
-var cliente = false // Si el usuario confirma que es cliente del banco en el simulador, tiene un interés anual especial del 9% (Falta la clase de DOM)
+var anos = 5 // Acá iría la cantidad de años en el cual devolvería el préstamo el usuario (Manipular HTML)
+var cuotaMensual // Valor de cuota mensual. Se modifica en tiempo real de acuerdo a los parámetros que elija el usuario. (Manipular HTML)
+var comision // Porcentaje de comisión de acuerdo al monto del préstamo. (Manipular HTML)
+var cliente = false // Si el usuario confirma que es cliente del banco en el simulador, tiene un interés anual especial del 9% (Manipular HTML)
+
+var planes = [
+  {
+    nombre: "Plan Basico",
+    monto: 10000,
+    interesAnual: 15
+  },
+  {
+    nombre: "Plan Medium",
+    monto: 50000,
+    interesAnual: 17
+  },
+  {
+    nombre: "Plan Premium",
+    monto: 100000,
+    interesAnual: 21
+  }
+]
+
+console.log("Cantidad de planes: " + planes.length)
 
 function simuladorPrestamo() {
 
@@ -30,10 +50,15 @@ function simuladorPrestamo() {
     /* Calculo total intereses */
 
     var totalInteres = (totalPrestamo - montoSolicitado).toFixed(2)
+
+    /* Calculo comision */
+
+    var comision = (totalPrestamo * 0.3).toFixed(2)
    
-    console.log(cuotaMensual)
-    console.log(totalPrestamo)
-    console.log(totalInteres)
+    console.log("Cuota mensual: " + cuotaMensual)
+    console.log("Monto total préstamo: " + totalPrestamo)
+    console.log("Intereses: " + totalInteres)
+    console.log("Comision: " + comision)
   }
 
 simuladorPrestamo()
