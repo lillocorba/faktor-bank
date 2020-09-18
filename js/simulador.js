d9af5bf0cd8e90b28e06bcbf71b6a71960fe4d12
Agustin Corbalan <agustin.corbalan1723@gmail.com>
2020-09-17 22:15:17 -0300

    Modificacion JS en simulador

--- js/simulador.js
+++ js/simulador.js
@@ -1,11 +1,51 @@
-var monto = 10000 // Acá iría el valor que elige el usuario en el simulador de la web (Manipular HTML)
-var interes = 12 // Porcentaje de interés anual fijado previamente
-var anos = 5 // Acá iría la cantidad de años en el cual devolvería el préstamo el usuario (Manipular HTML)
-var cuotaMensual // Valor de cuota mensual. Se modifica en tiempo real de acuerdo a los parámetros que elija el usuario. (Manipular HTML)
-var comision // Porcentaje de comisión de acuerdo al monto del préstamo. (Manipular HTML)
-var cliente = false // Si el usuario confirma que es cliente del banco en el simulador, tiene un interés anual especial del 9% (Manipular HTML)
+var monto = document.getElementById("rangeMonto").value
+var interes = 12
+var meses = document.getElementById("rangeMeses").value
+var cliente = document.getElementById("checkboxCliente").value
+var resultadoCuota = document.getElementById("resultadoCuota")
+var resultadoComision = document.getElementById("resultadoComision")
+var resultadoInteres = document.getElementById("resultadoInteres")
 
-var planes = [
+function simuladorPrestamo() {
+
+    /* Interes especial por ser cliente */
+    
+    if (cliente == true) {
+        interes = 9
+    }
+   
+    var montoSolicitado = parseFloat(monto)
+    var calculoInteres = (parseFloat(interes) / 100 / 12)
+
+   /* Calculo cuota mensual */
+
+    var x = Math.pow(1 + calculoInteres, meses)
+    var cuotaMensual = ((montoSolicitado * x * calculoInteres) / (x - 1)).toFixed(2)
+
+    /* Calculo monto total préstamo */
+
+    var totalPrestamo = (cuotaMensual * meses).toFixed(2)
+
+    /* Calculo total intereses */
+
+    var totalInteres = (totalPrestamo - montoSolicitado).toFixed(2)
+
+    /* Calculo comision */
+
+    var comision = (totalPrestamo * 0.3).toFixed(2)
+
+    resultadoCuota.innerHTML = "$" + cuotaMensual
+    resultadoInteres.innerHTML = "Interes: " + "$" + totalInteres
+    resultadoComision.innerHTML = "Comision: " + "$" + comision
+
+    console.log(cuotaMensual)
+    console.log(totalInteres)
+    console.log(comision)
+  }
+
+  simuladorPrestamo()
+
+  /* var planes = [
   {
     nombre: "Plan Basico",
     monto: 10000,
@@ -21,44 +61,4 @@ var planes = [
     monto: 100000,
     interesAnual: 21
   }
-]
-
-console.log("Cantidad de planes: " + planes.length)
-
-function simuladorPrestamo() {
-
-    /* Interes especial por ser cliente */
-    
-    if (cliente == true) {
-        interes = 9
-    }
-   
-    var montoSolicitado = parseFloat(monto)
-    var calculoInteres = (parseFloat(interes) / 100 / 12)
-    var calculoPagos = parseFloat(anos) * 12
-
-   /* Calculo cuota mensual */
-
-    var x = Math.pow(1 + calculoInteres, calculoPagos)
-    var cuotaMensual = ((montoSolicitado * x * calculoInteres) / (x - 1)).toFixed(2)
-
-
-    /* Calculo monto total préstamo */
-
-    var totalPrestamo = (cuotaMensual * calculoPagos).toFixed(2)
-
-    /* Calculo total intereses */
-
-    var totalInteres = (totalPrestamo - montoSolicitado).toFixed(2)
-
-    /* Calculo comision */
-
-    var comision = (totalPrestamo * 0.3).toFixed(2)
-   
-    console.log("Cuota mensual: " + cuotaMensual)
-    console.log("Monto total préstamo: " + totalPrestamo)
-    console.log("Intereses: " + totalInteres)
-    console.log("Comision: " + comision)
-  }
-
-simuladorPrestamo()
+] */
