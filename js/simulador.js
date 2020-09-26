ff446cfad39a5973038ad55d6459a293c5159d58
Agustin Corbalan <agustin.corbalan1723@gmail.com>
2020-09-26 14:46:58 -0300

    JSON y Storage JS

--- js/simulador.js
+++ js/simulador.js
@@ -6,17 +6,20 @@ var resultadoCuota = document.getElementById("resultadoCuota")
 var resultadoComision = document.getElementById("resultadoComision")
 var resultadoInteres = document.getElementById("resultadoInteres")
 var resultadoCliente = document.getElementById("checkboxCliente").checked
+var boton = document.getElementById("btnActualizar")
+
+boton.addEventListener("click", simuladorPrestamo)
 
 function simuladorPrestamo() {
 
-  var monto = document.getElementById("rangeMonto").value
-  var interes = 12
-  var meses = document.getElementById("rangeMeses").value
-  var cliente = document.getElementById("checkboxCliente").value
-  var resultadoCuota = document.getElementById("resultadoCuota")
-  var resultadoComision = document.getElementById("resultadoComision")
-  var resultadoInteres = document.getElementById("resultadoInteres")
-  var resultadoCliente = document.getElementById("checkboxCliente").checked
+  let monto = document.getElementById("rangeMonto").value
+  let interes = 12
+  let meses = document.getElementById("rangeMeses").value
+  let cliente = document.getElementById("checkboxCliente").value
+  let resultadoCuota = document.getElementById("resultadoCuota")
+  let resultadoComision = document.getElementById("resultadoComision")
+  let resultadoInteres = document.getElementById("resultadoInteres")
+  let resultadoCliente = document.getElementById("checkboxCliente").checked
 
   /* Interes especial por ser cliente */
 
@@ -26,47 +29,43 @@ function simuladorPrestamo() {
     interes = 12
   }
 
-  var montoSolicitado = parseFloat(monto)
-  var calculoInteres = (parseFloat(interes) / 100 / 12)
+  let montoSolicitado = parseFloat(monto)
+  let calculoInteres = (parseFloat(interes) / 100 / 12)
 
   /* Calculo cuota mensual */
 
-  var x = Math.pow(1 + calculoInteres, meses)
-  var cuotaMensual = ((montoSolicitado * x * calculoInteres) / (x - 1)).toFixed(2)
+  let x = Math.pow(1 + calculoInteres, meses)
+  let cuotaMensual = ((montoSolicitado * x * calculoInteres) / (x - 1)).toFixed(2)
 
   /* Calculo monto total préstamo */
 
-  var totalPrestamo = (cuotaMensual * meses).toFixed(2)
+  let totalPrestamo = (cuotaMensual * meses).toFixed(2)
 
   /* Calculo total intereses */
 
-  var totalInteres = (totalPrestamo - montoSolicitado).toFixed(2)
+  let totalInteres = (totalPrestamo - montoSolicitado).toFixed(2)
 
   /* Calculo comision */
 
-  var comision = (totalPrestamo * 0.3).toFixed(2)
+  let comision = (totalPrestamo * 0.3).toFixed(2)
+
+  /* Trasladar los resultados a Pantalla */
 
   resultadoCuota.innerHTML = "$" + cuotaMensual
   resultadoInteres.innerHTML = "Intereses: " + "$" + totalInteres
   resultadoComision.innerHTML = "Comision: " + "$" + comision
+
+  /* Pasar los resultados a JSON y guardarlos en Local Storage */
+
+  let resultadoPrestamoTotal = {
+    Cuota: cuotaMensual,
+    Comision: comision,
+    Interés: totalInteres
+  }
+
+  let json = JSON.stringify(resultadoPrestamoTotal)
+
+  localStorage.setItem('Último cálculo: ', json)
 }
 
 simuladorPrestamo()
-
-/* var planes = [
-{
-  nombre: "Plan Basico",
-  monto: 10000,
-  interesAnual: 15
-},
-{
-  nombre: "Plan Medium",
-  monto: 50000,
-  interesAnual: 17
-},
-{
-  nombre: "Plan Premium",
-  monto: 100000,
-  interesAnual: 21
-}
-] */
